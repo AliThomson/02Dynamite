5e867cc4edafbc6e5083fd93791366dbd2e9d645
Alison Thomson <Alison.Thomson@softwire.com>
2022-01-19 13:17:00 +0000

    Added counters for Ds played, plus conditional logic in Switch. Not currently working!

--- AliIjaBot.js
+++ AliIjaBot.js
@@ -4,21 +4,19 @@ class Bot {
             return 'D';
         }
         const p1DynamitesPlayed = gameState.rounds.filter(round => round.p1 === "D").length;
+        const p2DynamitesPlayed = gameState.rounds.filter(round => round.p2 === "D").length;
+        const p2WaterfallsPlayed = gameState.rounds.filter(round => round.p2 === "W").length;
 
         const lastOppMove = gameState.rounds[gameState.rounds.length-1].p2;
 
         switch(lastOppMove) {
-            case "R": if (p1DynamitesPlayed < 100) {
-                return "D"
-             } else {
-                return "P"
-             };
+            /*case "R": return (p1DynamitesPlayed < 100 ?  "D": "P");*/
+            case "R": return (p1DynamitesPlayed < 100 ?  "D": "P");
             case "S":
                 return "R";
             case "P":
                 return "S";
-            case "D":
-                return "W";
+            case "D": return (p2DynamitesPlayed < 100 ?  "W": "P");
             case "W":
                 return "R";
         }
