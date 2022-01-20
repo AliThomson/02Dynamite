b26b2fc019eadb38597247c52dd63631bd0e33ff
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-01-20 09:51:15 +0000

    add frequence

--- AliIjaBot.js
+++ AliIjaBot.js
@@ -1,18 +1,23 @@
 class Bot {
-    makeMove(gamestate) {
-        if (gamestate.rounds.length === 0) {
+    makeMove(gameState) {
+        if (gameState.rounds.length === 0) {
             return 'D';
         }
-        const probabilityArr {"P": 0,"S": 0,"R": 0, "D" : 0, "W" :0};
-        for (prob in probabilityArr) {
-            probabilityArr[prob] = gameState.rounds.filter(round => round.p1 === prob).length;
+        
+        frequencyArr =[{name: "P", value: 0},{name: "S", value: 0},{name: "R", value : 0}, {name:"D",value: 0}, {name:"W", value :0}]
+
+        for (freq in frequencyArr) {
+            frequencyArr[freq].value = gameState.rounds.filter(round => round.p1 === frequencyArr[freq].name).length;
         }
+
+        frequencyArr.sort(function (a,b) {return a.value-b.value;});  //frequencyArr[0].name - element with lower frequency
         
         const p1DynamitesPlayed = gameState.rounds.filter(round => round.p1 === "D").length;
 
-        const lastOppMove = gameState.rounds[gameState.rounds.length-1].p2;
+        //const lastOppMove = gameState.rounds[gameState.rounds.length-1].p2;
+        predictedOppMove=frequencyArr[0].name;
 
-        switch(lastOppMove) {
+        switch(predictedOppMove) {
             case "R": 
                 return (p1DynamitesPlayed < 100 ?  "D": "P");
             case "S":
@@ -30,6 +35,17 @@ class Bot {
 
 module.exports = new Bot();
 
+
+// probabilityArr =[{name: "P", value: 4},{name: "S", value: 23},{name: "R", value : 12}, {name:"D",value: 10}, {name:"W", value :0}]
+
+// probabilityArr.sort(function (a,b) {return a.value-b.value;});
+
+// for (prob in probabilityArr) {
+//                 //probabilityArr[prob] = gameState.rounds.filter(round => round.p1 === prob).length;
+//                 probabilityArr[prob].value++;
+//                 console.log(probabilityArr[prob].value);
+//             }
+// console.log(probabilityArr[0].name);
 
 
 
